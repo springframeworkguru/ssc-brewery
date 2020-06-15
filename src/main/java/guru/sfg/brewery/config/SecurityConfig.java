e7ab562fb06f1801e8fd23d4091a6cd2f499fd8f
John Thompson <john@springframework.guru>
2020-06-15 17:01:08 -0400

    adding fluent config example

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -41,6 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user")
                 .password("{noop}password")
                 .roles("USER");
+
+        auth.inMemoryAuthentication().withUser("scott").password("{noop}tiger").roles("CUSTOMER");
     }
 
     //    @Override
--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -23,6 +23,14 @@ public class BeerControllerIT extends BaseIT{
     }
 
     @Test
+    void initCreationFormWithScott() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("scott", "tiger")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
     void findBeers() throws Exception{
         mockMvc.perform(get("/beers/find"))
                 .andExpect(status().isOk())
