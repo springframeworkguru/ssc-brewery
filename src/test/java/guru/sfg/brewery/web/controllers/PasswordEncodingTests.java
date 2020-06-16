8e2d7db9d8a9b2233e975d0ed0f9a320fe17c326
John Thompson <john@springframework.guru>
2020-06-16 15:23:03 -0400

    adding MD5 example

---
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTests.java
@@ -0,0 +1,21 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.DigestUtils;
+
+/**
+ * Created by jt on 6/16/20.
+ */
+public class PasswordEncodingTests {
+
+    static final String PASSWORD = "password";
+
+    @Test
+    void hashingExample() {
+        System.out.println(DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
+        System.out.println(DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
+
+        String salted = PASSWORD + "ThisIsMySALTVALUE";
+        System.out.println(DigestUtils.md5DigestAsHex(salted.getBytes()));
+    }
+}
