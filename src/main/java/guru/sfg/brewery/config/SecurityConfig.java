dfe5d45b6f52608c9de5c282c979ec4e51e5ca68
John Thompson <john@springframework.guru>
2020-06-19 15:25:58 -0400

    adding debug, remove crsf

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
                 http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
-                        UsernamePasswordAuthenticationFilter.class);
+                        UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable();
 
                 http
                 .authorizeRequests(authorize -> {
