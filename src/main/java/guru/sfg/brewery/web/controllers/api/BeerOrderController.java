4c2be47845ad817284eb1d080638a59c0093ce95
John Thompson <john@springframework.guru>
2020-07-02 17:26:33 -0400

    adding order controller

---
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright 2019 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package guru.sfg.brewery.web.controllers.api;
+
+import guru.sfg.brewery.services.BeerOrderService;
+import guru.sfg.brewery.web.model.BeerOrderDto;
+import guru.sfg.brewery.web.model.BeerOrderPagedList;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * Beer Order Controller
+ */
+@RequestMapping("/api/v1/customers/{customerId}/")
+@RestController
+public class BeerOrderController {
+
+    private static final Integer DEFAULT_PAGE_NUMBER = 0;
+    private static final Integer DEFAULT_PAGE_SIZE = 25;
+
+    private final BeerOrderService beerOrderService;
+
+    public BeerOrderController(BeerOrderService beerOrderService) {
+        this.beerOrderService = beerOrderService;
+    }
+
+    @GetMapping("orders")
+    public BeerOrderPagedList listOrders(@PathVariable("customerId") UUID customerId,
+                                         @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                         @RequestParam(value = "pageSize", required = false) Integer pageSize){
+
+        if (pageNumber == null || pageNumber < 0){
+            pageNumber = DEFAULT_PAGE_NUMBER;
+        }
+
+        if (pageSize == null || pageSize < 1) {
+            pageSize = DEFAULT_PAGE_SIZE;
+        }
+
+        return beerOrderService.listOrders(customerId, PageRequest.of(pageNumber, pageSize));
+    }
+
+    @PostMapping("orders")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerOrderDto placeOrder(@PathVariable("customerId") UUID customerId, @RequestBody BeerOrderDto beerOrderDto){
+        return beerOrderService.placeOrder(customerId, beerOrderDto);
+    }
+
+    @GetMapping("orders/{orderId}")
+    public BeerOrderDto getOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
+        return beerOrderService.getOrderById(customerId, orderId);
+    }
+
+    @PutMapping("/orders/{orderId}/pickup")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void pickupOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
+        beerOrderService.pickupOrder(customerId, orderId);
+    }
+}
