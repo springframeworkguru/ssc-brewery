c4aee1d2e0d27d79e105c75e265e706efb2d0404
John Thompson <john@springframework.guru>
2020-07-08 18:02:02 -0400

    fixing test

--- src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.web.controllers;
 import guru.sfg.brewery.repositories.BeerInventoryRepository;
 import guru.sfg.brewery.repositories.BeerRepository;
 import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.services.BeerOrderService;
 import guru.sfg.brewery.services.BeerService;
 import guru.sfg.brewery.services.BreweryService;
 import org.junit.jupiter.api.Test;
@@ -32,6 +33,9 @@ public class IndexControllerIT extends BaseIT {
 
     @MockBean
     BeerService beerService;
+
+    @MockBean
+    BeerOrderService beerOrderService;
 
     @Test
     void testGetIndexSlash() throws Exception{
