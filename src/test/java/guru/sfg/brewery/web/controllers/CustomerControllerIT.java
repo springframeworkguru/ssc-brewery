86f5e84eea499f696042cf0e7250728c4bd373f5
John Thompson <john@springframework.guru>
2020-07-08 18:19:25 -0400

    adding CSRF to tests

--- src/test/java/guru/sfg/brewery/web/controllers/CustomerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/CustomerControllerIT.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -53,7 +54,7 @@ public class CustomerControllerIT extends BaseIT {
         @Rollback
         @Test
         void processCreationForm() throws Exception{
-            mockMvc.perform(post("/customers/new")
+            mockMvc.perform(post("/customers/new").with(csrf())
                     .param("customerName", "Foo Customer")
                     .with(httpBasic("spring", "guru")))
                     .andExpect(status().is3xxRedirection());
@@ -71,7 +72,7 @@ public class CustomerControllerIT extends BaseIT {
 
         @Test
         void processCreationFormNOAUTH() throws Exception{
-            mockMvc.perform(post("/customers/new")
+            mockMvc.perform(post("/customers/new").with(csrf())
                     .param("customerName", "Foo Customer"))
                     .andExpect(status().isUnauthorized());
         }
