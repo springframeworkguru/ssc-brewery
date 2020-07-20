e55ebcd1e831f1785828b60c9cd5fec591b6f328
John Thompson <john@springframework.guru>
2020-07-20 16:14:32 -0400

    adding failure logging

---
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
@@ -0,0 +1,37 @@
+package guru.sfg.brewery.security.listeners;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 7/20/20.
+ */
+@Slf4j
+@Component
+public class AuthenticationFailureListener {
+
+    @EventListener
+    public void listen(AuthenticationFailureBadCredentialsEvent event){
+        log.debug("Login failure");
+
+        if(event.getSource() instanceof UsernamePasswordAuthenticationToken){
+            UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
+
+            if(token.getPrincipal() instanceof String){
+                log.debug("Attempted Username: " + token.getPrincipal());
+            }
+
+            if(token.getDetails() instanceof WebAuthenticationDetails){
+                WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
+
+                log.debug("Source IP: " + details.getRemoteAddress());
+            }
+        }
+
+
+    }
+}
