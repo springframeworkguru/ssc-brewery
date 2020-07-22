a8fd822846ee8d191ab1ff3c9b9c5e98124a4d89
Davé <cookieMr@users.noreply.github.com>
2020-07-22 16:22:06 +0200

    Adding User Details Service (#10)

--- src/main/java/guru/sfg/brewery/config/SpringSecurityConfiguration.java
+++ src/main/java/guru/sfg/brewery/config/SpringSecurityConfiguration.java
@@ -1,11 +1,17 @@
 package guru.sfg.brewery.config;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +21,11 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
             {"/", "/login", "/beers/find", "/beers*", "/webjars/**", "/resources/**"};
     public static final String[] PUBLIC_ANT_GET_URLS = {"/api/v1/beer/**"};
     public static final String[] PUBLIC_MVC_GET_URLS = {"/api/v1/beerUpc/{upc}"};
+
+    @Value("${spring.security.user.name:}")
+    String adminUserName;
+    @Value("${spring.security.user.password:}")
+    String adminPassword;
 
     @Override
     protected void configure(@NotNull HttpSecurity http) throws Exception {
@@ -28,6 +39,24 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().and()
                 .httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected @NotNull UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username(adminUserName)
+                .password("{noop}" + adminPassword)
+                .roles("ADMIN")
+                .build();
+
+        UserDetails aUser = User.builder()
+                .username("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, aUser);
     }
 
 }
--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -1,8 +1,14 @@
 package guru.sfg.brewery.web.controllers;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
+
+import java.util.stream.Stream;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
@@ -50,6 +56,22 @@ public class BeerControllerIT extends AbstractBaseIT {
         mockMvc.perform(get("/beers/find").with(httpBasic(user, password)))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    private static @NotNull Stream<Arguments> usersAndPasswords() {
+        return Stream.of(
+                Arguments.of("spring", "secret"),
+                Arguments.of("user", "password")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("usersAndPasswords")
+    void initCreationForm(@NotNull String user, @NotNull String password) throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic(user, password)))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
                 .andExpect(model().attributeExists("beer"));
     }
 
