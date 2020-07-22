7fbdf9dcd906af20579723ad00f968076c11fa28
Davé <cookieMr@users.noreply.github.com>
2020-07-22 15:27:53 +0200

    Permit all to find beer by REST API (#8)

--- src/main/java/guru/sfg/brewery/config/SpringSecurityConfiguration.java
+++ src/main/java/guru/sfg/brewery/config/SpringSecurityConfiguration.java
@@ -2,6 +2,7 @@ package guru.sfg.brewery.config;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,13 +11,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    public static final String[] PUBLIC_URLS = {"/", "/login", "/beers/find", "/beers*", "/webjars/**", "/resources/**"};
+    public static final String[] PUBLIC_URLS =
+            {"/", "/login", "/beers/find", "/beers*", "/webjars/**", "/resources/**"};
+    public static final String[] PUBLIC_GET_URLS = {"/api/v1/beer/**"};
 
     @Override
     protected void configure(@NotNull HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorize ->
-                        authorize.antMatchers(PUBLIC_URLS).permitAll())
+                        authorize.antMatchers(PUBLIC_URLS).permitAll()
+                                .antMatchers(HttpMethod.GET, PUBLIC_GET_URLS).permitAll())
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
---
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -0,0 +1,31 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BeerRestControllerIT extends AbstractBaseIT {
+
+    @Test
+    void findBeerWithoutAuth() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerWithAnonymous() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/").with(anonymous()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
+                .andExpect(status().isOk());
+    }
+
+}
