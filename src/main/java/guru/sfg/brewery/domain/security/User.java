8fa9fccdce04af8e29f16a2a487aee4942c5f608
John Thompson <john@springframework.guru>
2020-07-23 16:54:27 -0400

    updating user for 2FA

--- src/main/java/guru/sfg/brewery/domain/security/User.java
+++ src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -85,6 +85,14 @@ public class User implements UserDetails, CredentialsContainer {
     @Builder.Default
     private Boolean enabled = true;
 
+    @Builder.Default
+    private Boolean userGoogle2fa = false;
+
+    private String google2FaSecret;
+
+    @Transient
+    private Boolean google2faRequired = true;
+
     @Override
     public void eraseCredentials() {
         this.password = null;
--- src/main/java/guru/sfg/brewery/security/UserUnlockService.java
+++ src/main/java/guru/sfg/brewery/security/UserUnlockService.java
@@ -21,7 +21,7 @@ public class UserUnlockService {
 
     private final UserRepository userRepository;
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 300000)
     public void unlockAccounts(){
         log.debug("Running Unlock Accounts");
 
