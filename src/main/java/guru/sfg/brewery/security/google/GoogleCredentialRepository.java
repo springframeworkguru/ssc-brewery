529acdf2194e862b877610364c0542c265997455
John Thompson <john@springframework.guru>
2020-07-23 17:30:12 -0400

    adding google auth dep, and persistence

---
+++ src/main/java/guru/sfg/brewery/security/google/GoogleCredentialRepository.java
@@ -0,0 +1,36 @@
+package guru.sfg.brewery.security.google;
+
+import com.warrenstrange.googleauth.ICredentialRepository;
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by jt on 7/23/20.
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class GoogleCredentialRepository implements ICredentialRepository {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public String getSecretKey(String userName) {
+        User user = userRepository.findByUsername(userName).orElseThrow();
+
+        return user.getGoogle2FaSecret();
+    }
+
+    @Override
+    public void saveUserCredentials(String userName, String secretKey, int validationCode, List<Integer> scratchCodes) {
+        User user = userRepository.findByUsername(userName).orElseThrow();
+        user.setGoogle2FaSecret(secretKey);
+        user.setUserGoogle2fa(true);
+        userRepository.save(user);
+    }
+}
