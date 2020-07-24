fd4f4471e864a7b3295adb61fe292965af3378df
Davé <cookieMr@users.noreply.github.com>
2020-07-24 15:14:05 +0200

    Loading init data for authentication into DB (#20)

---
+++ src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
@@ -0,0 +1,62 @@
+package guru.sfg.brewery.bootstrap;
+
+import guru.sfg.brewery.domain.security.Authority;
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.AuthorityRepository;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class UserDataLoader implements CommandLineRunner {
+
+    private final AuthorityRepository authorityRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${spring.security.user.name:}")
+    String adminUser;
+    @Value("${spring.security.user.password:}")
+    String password;
+
+    @Override
+    public void run(@Nullable String... args) {
+        if (authorityRepository.count() == 0) {
+            loadUserData();
+        }
+    }
+
+    private void loadUserData() {
+        Authority admin = authorityRepository.save(Authority.builder().role("ADMIN").build());
+        Authority user = authorityRepository.save(Authority.builder().role("USER").build());
+        Authority customer = authorityRepository.save(Authority.builder().role("CUSTOMER").build());
+
+        userRepository.save(User.builder()
+                .username(adminUser)
+                .password(passwordEncoder.encode(password))
+                .authority(admin)
+                .build());
+
+        userRepository.save(User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .authority(user)
+                .build());
+
+        userRepository.save(User.builder()
+                .username("scott")
+                .password(passwordEncoder.encode("tiger"))
+                .authority(customer)
+                .build());
+
+        log.debug("Loaded {} authorities and {} users.", authorityRepository.count(), userRepository.count());
+    }
+
+}
