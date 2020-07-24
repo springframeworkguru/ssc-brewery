8ee852ecf0f31625deabc85b9493de3e5edf2459
John Thompson <john@springframework.guru>
2020-07-24 10:42:08 -0400

    adding verify registration

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -41,15 +41,28 @@ public class UserController {
         return "user/register2fa";
     }
 
+    @PostMapping
+    public String confirm2Fa(@RequestParam Integer verifyCode){
+
+        User user = getUser();
+
+        log.debug("Entered Code is:" + verifyCode);
+
+        if (googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)) {
+            User savedUser = userRepository.findById(user.getId()).orElseThrow();
+            savedUser.setUserGoogle2fa(true);
+            userRepository.save(savedUser);
+
+            return "/index";
+        } else {
+            // bad code
+            return "user/register2fa";
+        }
+    }
+
     private User getUser() {
         return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
-    @PostMapping
-    public String confirm2Fa(@RequestParam Integer verifyCode){
 
-        //todo - impl
-
-        return "index";
-    }
 }
