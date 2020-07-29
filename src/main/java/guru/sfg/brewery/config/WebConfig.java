a5bcbb9befb32759428490fc552610650d356f3a
John Thompson <john@springframework.guru>
2020-07-28 20:26:40 -0400

    adding annotation configuration

---
+++ src/main/java/guru/sfg/brewery/config/WebConfig.java
@@ -0,0 +1,16 @@
+package guru.sfg.brewery.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by jt on 7/28/20.
+ */
+//@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        //registry.
+//        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
+//    }
+}
--- src/main/java/guru/sfg/brewery/web/controllers/api/BeerRestController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerRestController.java
@@ -39,6 +39,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/")
