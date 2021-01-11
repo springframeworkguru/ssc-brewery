28c088088cd32620b95c90441ca21aeb334ead51
skrzeminski <sebastian.krzeminski93@gmail.com>
2021-01-11 20:58:09 +0100

    added BeerOrderController

---
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
@@ -0,0 +1,60 @@
+package guru.sfg.brewery.web.controllers.api;
+
+import guru.sfg.brewery.services.BeerOrderService;
+import guru.sfg.brewery.web.model.BeerOrderDto;
+import guru.sfg.brewery.web.model.BeerOrderPagedList;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * Beer Order Controller
+ */
+@RequestMapping("/api/v1/customers/{customerId}/")
+@RestController
+public class BeerOrderController {
+
+    private static final Integer DEFAULT_PAGE_NUMBER = 0;
+    private static final Integer DEFAULT_PAGE_SIZE = 25;
+
+    private final BeerOrderService beerOrderService;
+
+    public BeerOrderController(BeerOrderService beerOrderService) {
+        this.beerOrderService = beerOrderService;
+    }
+
+    @GetMapping("orders")
+    public BeerOrderPagedList listOrders(@PathVariable("customerId") UUID customerId,
+                                         @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                         @RequestParam(value = "pageSize", required = false) Integer pageSize){
+
+        if (pageNumber == null || pageNumber < 0){
+            pageNumber = DEFAULT_PAGE_NUMBER;
+        }
+
+        if (pageSize == null || pageSize < 1) {
+            pageSize = DEFAULT_PAGE_SIZE;
+        }
+
+        return beerOrderService.listOrders(customerId, PageRequest.of(pageNumber, pageSize));
+    }
+
+    @PostMapping("orders")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerOrderDto placeOrder(@PathVariable("customerId") UUID customerId, @RequestBody BeerOrderDto beerOrderDto){
+        return beerOrderService.placeOrder(customerId, beerOrderDto);
+    }
+
+    @GetMapping("orders/{orderId}")
+    public BeerOrderDto getOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
+        return beerOrderService.getOrderById(customerId, orderId);
+    }
+
+    @PutMapping("/orders/{orderId}/pickup")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void pickupOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
+        beerOrderService.pickupOrder(customerId, orderId);
+    }
+}
