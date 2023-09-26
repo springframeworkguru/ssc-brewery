a25d93416c725f0118b83969bf3d65264438ff3f
DeevS <deev@advantum.pro>
2023-09-26 19:37:52 +0300

    Custom Delegating Password Encoder

--- src/main/java/guru/sfg/brewery/configuration/SecurityConfiguration.java
+++ src/main/java/guru/sfg/brewery/configuration/SecurityConfiguration.java
@@ -1,17 +1,13 @@
 package guru.sfg.brewery.configuration;
 
+import guru.sfg.brewery.security.CustomEncoderFactories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -76,7 +72,7 @@ public class SecurityConfiguration {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return  PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return CustomEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
